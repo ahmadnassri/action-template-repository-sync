00bc8d41da13623d084d0ea90a006d5a0f573fbc
Ahmad Nassri <ahmad@ahmadnassri.com>
2021-05-14 16:51:49 -0400

    chore(debug): include debug messages for tree creation

--- action/lib/index.js
+++ action/lib/index.js
@@ -136,11 +136,15 @@ export default async function ({ token, dry, config: path }) {
         repo
       })
 
+      core.debug(`${repo}: default branch: ${default_branch}`)
+
       // Grab the latest commit
       const { data: [{ sha, commit: { tree } }] } = await octokit.request('GET /repos/{owner}/{repo}/commits?per_page=1', {
         owner: github.context.repo.owner,
         repo
       })
+
+      core.debug(`${repo}: latest commit: { sha: ${sha}, tree.sha: ${tree.sha} }`)
 
       // Make a new tree for the deltas
       const { data: newTree } = await octokit.request('POST /repos/{owner}/{repo}/git/trees', {
@@ -150,6 +154,8 @@ export default async function ({ token, dry, config: path }) {
         tree: newContent
       })
 
+      core.debug(`${repo}: new tree: ${newTree.sha}`)
+
       // Make a new commit with the delta tree
       const { data: newCommit } = await octokit.request('POST /repos/{owner}/{repo}/git/commits', {
         owner: github.context.repo.owner,
@@ -158,6 +164,8 @@ export default async function ({ token, dry, config: path }) {
         tree: newTree.sha,
         parents: [sha]
       })
+
+      core.debug(`${repo}: new commit: ${newCommit.sha}`)
 
       // Set HEAD of default branch to the new commit
       await octokit.request('PATCH /repos/{owner}/{repo}/git/refs/{ref}', {
