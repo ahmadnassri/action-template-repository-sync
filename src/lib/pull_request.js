4bc27f1b7bc860727e378817eda1900e1072802b
Ahmad Nassri <ahmad@ahmadnassri.com>
2022-08-24 16:47:18 +0000

    feat: updates existing PR comments

--- src/lib/pull_request.js
+++ src/lib/pull_request.js
@@ -4,17 +4,17 @@ import { createPatch } from 'diff'
 import github from '@actions/github'
 
 export default async function (octokit, { changedRepositories, localFiles }) {
-  const body = ['##### Template Repository Sync Report']
+  const output = ['## Template Repository Sync Report']
 
   if (changedRepositories.size === 0) {
-    body.push('> no changes to sync')
+    output.push('> no changes to sync')
   } else {
     const count = [...changedRepositories.values()].map(x => [...x.values()]).flat().length
 
-    body.push(`> Found ${count} files to update in ${changedRepositories.size} repositories`, '')
+    output.push(`> Found ${count} files to update in ${changedRepositories.size} repositories`, '')
 
     for (const [repo, remoteFiles] of changedRepositories.entries()) {
-      body.push(`<details><summary>${repo}</summary>`, '')
+      output.push(`<details><summary>${repo}</summary>`, '')
 
       for (const [path, remoteContent] of remoteFiles.entries()) {
         const before = remoteContent.toString('utf8')
@@ -22,18 +22,26 @@ export default async function (octokit, { changedRepositories, localFiles }) {
 
         const patch = createPatch(path, before, after)
 
-        body.push('```diff', patch, '```', '', '')
+        output.push('```diff', patch, '```', '', '')
       }
 
-      body.push('</details>', '')
+      output.push('</details>', '')
     }
   }
 
-  const { payload: { pull_request } } = github.context
+  const body = output.join('\n')
+  const { issue: { number: issue_number } } = github.context
+
+  // retrieve existing comments for the PR
+  const { data: comments } = await octokit.rest.issues.listComments({ ...github.context.repo, issue_number })
+
+  // find existing comment
+  const old = comments.find(comment => comment.body.includes('## Template Repository Sync Report'))
+
   // update PR
-  await octokit.rest.issues.createComment({
-    ...github.context.repo,
-    issue_number: pull_request.number,
-    body: body.join('\n')
-  })
+  if (old) {
+    await octokit.rest.issues.updateComment({ ...github.context.repo, comment_id: old.id, body })
+  } else {
+    await octokit.rest.issues.createComment({ ...github.context.repo, issue_number, body })
+  }
 }
